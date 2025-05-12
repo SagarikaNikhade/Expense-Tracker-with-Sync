c5900d742fd6596deaa723360a7a4583dd80a5a6
wave2211 <sagarikanikhade073@gmail.com>
2025-05-12 12:52:37 +0530

    basic setup of backend is done

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const { connection } = require("./config/db");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Expense Tracker!");
+});
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log(`Server is running at port ${process.env.port}`);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
